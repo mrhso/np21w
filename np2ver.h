595061544ed913d10f6e1ab637c5bf879982f674
mrhso <liu.songquan@live.cn>
2020-04-08 12:07:00 +0800

    np21w-0.86-rev72

--- np2ver.h
+++ np2ver.h
@@ -4,9 +4,9 @@
  */
 
 #if defined(SUPPORT_IA32_HAXM)
-#define	NP2VER_CORE			"ver.0.86 rev.71HAX"
+#define	NP2VER_CORE			"ver.0.86 rev.72HAX"
 #else
-#define	NP2VER_CORE			"ver.0.86 rev.71"
+#define	NP2VER_CORE			"ver.0.86 rev.72"
 #endif
 
 // #define	NP2VER_WIN9X
